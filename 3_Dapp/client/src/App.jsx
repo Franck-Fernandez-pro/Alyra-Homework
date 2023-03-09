c732bd94cf3804e40348c8937e9d80c558b9be69
Franck Fernandez <franck.fernandez@gmail.com>
2023-03-09 18:39:01 +0100

    Remove boilerplate

--- 3_Dapp/client/src/App.jsx
+++ 3_Dapp/client/src/App.jsx
@@ -1,22 +1,10 @@
-import { EthProvider } from "./contexts/EthContext";
-import Intro from "./components/Intro/";
-import Setup from "./components/Setup";
-import Demo from "./components/Demo";
-import Footer from "./components/Footer";
+import { EthProvider } from './contexts/EthContext';
 
 function App() {
   return (
     <EthProvider>
-      <div id="App">
-        <div className="container">
-          <Intro />
-          <hr />
-          <Setup />
-          <hr />
-          <Demo />
-          <hr />
-          <Footer />
-        </div>
+      <div>
+        <button className="btn btn-primary">Toto</button>
       </div>
     </EthProvider>
   );
