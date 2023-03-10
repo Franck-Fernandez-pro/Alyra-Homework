945397aad20fddd88893b2568402d2ebcd92c11d
Baptiste <avocalam@gmail.com>
2023-03-10 16:39:02 +0100

    add worflow component to app

--- 3_Dapp/client/src/App.jsx
+++ 3_Dapp/client/src/App.jsx
@@ -1,10 +1,12 @@
 import { EthProvider } from "./contexts/EthContext";
+import Workflow from "./components/Workflow";
 
 function App() {
   return (
     <EthProvider>
       <div>
         <button className="btn btn-primary">Toto</button>
+        <Workflow />
       </div>
     </EthProvider>
   );
