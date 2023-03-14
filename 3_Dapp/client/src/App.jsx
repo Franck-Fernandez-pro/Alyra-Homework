c2f626b1979e430db3cd973362e1afde2810fc76
Baptiste <avocalam@gmail.com>
2023-03-14 14:42:58 +0100

    add nav bar

--- 3_Dapp/client/src/App.jsx
+++ 3_Dapp/client/src/App.jsx
@@ -1,11 +1,13 @@
 import { EthProvider } from "./contexts/EthContext";
 import Workflow from "./components/Workflow";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <EthProvider>
       <div>
-        <button className="btn btn-primary">Toto</button>
+        {/* <button className="btn btn-primary">Toto</button> */}
+        <NavBar />
         <Workflow />
       </div>
     </EthProvider>
