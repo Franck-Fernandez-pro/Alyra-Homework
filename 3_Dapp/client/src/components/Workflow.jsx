51bca456a8096d5809e7402b76939dc8153bbb1e
Franck Fernandez <franck.fernandez@gmail.com>
2023-03-14 11:35:13 +0100

    Run prettier

--- 3_Dapp/client/src/components/Workflow.jsx
+++ 3_Dapp/client/src/components/Workflow.jsx
@@ -1,53 +1,66 @@
-import { useEffect, useState } from "react";
-import WorkflowStatus from "./workflowStatus";
-import useEth from "../contexts/EthContext/useEth";
+import { useEffect, useState } from 'react';
+import WorkflowStatus from './workflowStatus';
+import useEth from '../contexts/EthContext/useEth';
 
 function Workflow() {
-  const { state: { contract, accounts } } = useEth();
+  const {
+    state: { contract },
+  } = useEth();
 
   const [currentWorkflow, setCurrentWorkflow] = useState(0);
 
   useEffect(() => {
-    (async () => {
+    async function fetchEvents() {
       let oldEvents = await contract.getPastEvents('WorkflowStatusChange', {
         fromBlock: 0,
-        toBlock: 'latest'
+        toBlock: 'latest',
       });
-  
+
       if (oldEvents) {
-        setCurrentWorkflow(oldEvents[oldEvents.length - 1].returnValues.newStatus);
+        setCurrentWorkflow(
+          oldEvents[oldEvents.length - 1].returnValues.newStatus
+        );
       }
-  
-      await contract.events.WorkflowStatusChange({ fromBlock: "earliest" })
-        .on("data", event => {
-          setCurrentWorkflow(event.returnValues.newStatus)
-        })          
-        .on("changed", changed => console.log(changed))
-        .on("error", err => console.log(err))
-        .on("connected", str => console.log(str))
-    })();
-  }, [contract])
+
+      await contract.events
+        .WorkflowStatusChange({ fromBlock: 'earliest' })
+        .on('data', (event) => {
+          setCurrentWorkflow(event.returnValues.newStatus);
+        });
+    }
+
+    fetchEvents();
+  }, [contract]);
 
   const getStatus = (workflow) => {
-    console.log("current wf ", currentWorkflow, " comp wf ",  workflow);
+    console.log('current wf ', currentWorkflow, ' comp wf ', workflow);
     if (currentWorkflow == workflow) {
-      console.log("current")
-      return "current"
+      console.log('current');
+      return 'current';
     }
     if (currentWorkflow > workflow) {
-      console.log("done")
-      return "done"
+      console.log('done');
+      return 'done';
     }
-    console.log("current")
+    console.log('current');
 
-    return "pending";
+    return 'pending';
   };
 
   return (
     <div className="border">
-      <WorkflowStatus status={getStatus(0)} label="Enregistrement des élécteurs" />
-      <WorkflowStatus status={getStatus(1)} label="Enregistrement des proposition en cours" />
-      <WorkflowStatus status={getStatus(2)} label="Enregistrement des propositions fermé" />
+      <WorkflowStatus
+        status={getStatus(0)}
+        label="Enregistrement des élécteurs"
+      />
+      <WorkflowStatus
+        status={getStatus(1)}
+        label="Enregistrement des proposition en cours"
+      />
+      <WorkflowStatus
+        status={getStatus(2)}
+        label="Enregistrement des propositions fermé"
+      />
       <WorkflowStatus status={getStatus(3)} label="Session de vote en cours" />
       <WorkflowStatus status={getStatus(4)} label="Session de vote fermée" />
       <WorkflowStatus status={getStatus(5)} label="Résultats des votes" />
--- 3_Dapp/client/src/components/WorkflowStatus.jsx
+++ 3_Dapp/client/src/components/WorkflowStatus.jsx
@@ -1,25 +1,25 @@
 import React from 'react';
 
-function WorkflowStatus (props) {
+function WorkflowStatus(props) {
   const getStatusStyle = () => {
-    if (props.status === "done") { return "bg-success" }
-    if (props.status === "current") { return "bg-info" }
-    return "bg-neutral";
-  }
+    if (props.status === 'done') return 'bg-success';
+    if (props.status === 'current') return 'bg-info';
+    return 'bg-neutral';
+  };
 
   const getStatusText = () => {
-    if (props.status === "done") { return "Complété" }
-    if (props.status === "current") { return "En cours" }
-    return "En attente";
-  }
+    if (props.status === 'done') return 'Complété';
+    if (props.status === 'current') return 'En cours';
+    return 'En attente';
+  };
 
   return (
-    <div className="w-full flex">
-      <p className="w-full text-end pr-5">{props.label}</p>
-      <div className='flex w-full items-start pl-5'>
-        <div className={`text-white rounded-full px-2 ${getStatusStyle()}`}>
+    <div className="flex w-full">
+      <p className="w-full pr-5 text-end">{props.label}</p>
+      <div className="flex w-full items-start pl-5">
+        <div className={`rounded-full px-2 text-white ${getStatusStyle()}`}>
           {getStatusText()}
-        </div> 
+        </div>
       </div>
     </div>
   );
