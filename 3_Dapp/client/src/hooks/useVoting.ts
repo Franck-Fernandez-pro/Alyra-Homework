053b44e28382d98d47a939bd639b618299a52d84
Baptiste <avocalam@gmail.com>
2023-03-15 20:22:33 +0100

    add use contract to call contract

--- 3_Dapp/client/src/hooks/useVoting.ts
+++ 3_Dapp/client/src/hooks/useVoting.ts
@@ -1,9 +1,12 @@
 import { useState } from 'react';
 import { useContractEvent } from 'wagmi';
+import { useContract, useSigner } from 'wagmi'
 import artifact from '../contracts/Voting.json';
 
 export function useVoting() {
   const [currentWorkflow, setCurrentWorkflow] = useState<number>(0);
+  const { data: signerData } = useSigner();
+  
   useContractEvent({
     address: import.meta.env.VITE_VOTING_ADDR,
     abi: artifact.abi,
@@ -14,5 +17,11 @@ export function useVoting() {
     },
   });
 
-  return { currentWorkflow };
+  const voting = useContract({
+    address: import.meta.env.VITE_VOTING_ADDR,
+    abi: artifact.abi,
+    signerOrProvider: signerData,
+  })
+
+  return { currentWorkflow, voting };
 }
