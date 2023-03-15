90d8c450e7175c5a4bfb016b829f14d71998422a
Baptiste <avocalam@gmail.com>
2023-03-15 20:22:13 +0100

    add good role displaying

--- 3_Dapp/client/src/components/NavBar.tsx
+++ 3_Dapp/client/src/components/NavBar.tsx
@@ -1,26 +1,51 @@
 import { useEffect, useState } from 'react';
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 import { useSigner } from 'wagmi';
-
+import { useVoting } from '../hooks';
 
 function NavBar () {
   const [userAddress, setUserAddress] = useState("");
-  const [userStatus, setUserStatus] = useState("- - -");
+  const [userStatus, setUserStatus] = useState("");
 
   const {data: signer} = useSigner();
-  console.log('user addr', userAddress);
+  const { voting } = useVoting();
+  
 
   useEffect(() => {
     //@ts-ignore
     setUserAddress(signer?._address);
   }, [signer]);
 
-  // useEffect(() => {
-  //   if (contract) {
-  //     // const isOwner = await contract._checkOwner.call({from: userAddress});
-  //     console.log('isOwner', isOwner);
-  //   }
-  // }, [contract]);
+  useEffect(() => {
+    if (userAddress) {
+      getUserStatus();
+    }
+  }, [userAddress]);
+
+  const getUserStatus = async () => {
+    const ownerAddr = await voting?.owner.call();
+    if (ownerAddr === userAddress) {
+      setUserStatus("owner");
+      return;
+    }
+
+    try {
+      const userData = await voting?.getVoter(userAddress);
+      if (userData.isRegistered) {
+        setUserStatus("voter");
+        return;
+      }
+    } catch {
+      setUserStatus("guest");
+    }
+    setUserStatus("guest");
+  };
+
+  const statusTranslation = () => {
+    if (userStatus === "owner") { return "Propriétaire" }
+    if (userStatus === "voter") { return "Élécteur" }
+    return "Invité"
+  };
 
   return (
     <div className="navbar bg-base-100 flex justify-center">
@@ -43,7 +68,7 @@ function NavBar () {
           <ConnectButton />
           { userAddress &&
             <span>
-              Bonjour ! Vous êtes <b className="text-primary">owner</b>
+              Bonjour ! Vous êtes <b className="text-primary">{ statusTranslation() }</b>
             </span> 
           } 
         </div>
