55b6bdc614f6c068752d607616b76b7fe7f79a98
Baptiste <avocalam@gmail.com>
2023-03-16 15:02:12 +0100

    create hooks for account gestion

---
+++ 3_Dapp/client/src/hooks/useConnectedWallet.ts
@@ -0,0 +1,9 @@
+import { useSigner } from 'wagmi'
+
+export function useConnectedWallet() {
+  const { data: signer } = useSigner();
+  // @ts-ignore
+  const userAddress = signer?._address;
+
+  return { userAddress };
+}
