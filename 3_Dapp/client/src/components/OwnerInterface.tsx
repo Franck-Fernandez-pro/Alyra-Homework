e2da1d508e8255557037b9427a8ae93c55d358d5
Baptiste <avocalam@gmail.com>
2023-03-17 10:03:53 +0100

    add owner interface

---
+++ 3_Dapp/client/src/components/OwnerInterface.tsx
@@ -0,0 +1,11 @@
+import { ChangeEvent, useEffect, useState } from 'react';
+
+function OwnerInterface () {
+  return (
+    <div className="text-center">
+      Owner interface
+    </div>
+  );
+}
+
+export default OwnerInterface;
