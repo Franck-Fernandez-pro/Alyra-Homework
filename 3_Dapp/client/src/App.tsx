95fea8f0c2d72847f782800c4c05529d0cc6b38b
Baptiste <avocalam@gmail.com>
2023-03-17 09:58:53 +0100

    add react toastify

--- 3_Dapp/client/src/App.tsx
+++ 3_Dapp/client/src/App.tsx
@@ -2,14 +2,23 @@ import Workflow from './components/Workflow';
 import NavBar from './components/NavBar';
 import ActionsContainer from './components/ActionsContainer';
 import Results from './components/Results';
+import OwnerInterface from './components/OwnerInterface';
+import { ToastContainer } from 'react-toastify';
 
 function App() {
   return (
     <div>
+      <ToastContainer
+        position="bottom-center"
+        hideProgressBar
+      />
       <NavBar />
       <div className="flex gap-9 mt-20">
         <Workflow />
         <ActionsContainer />
+      </div>
+      <div className="mt-20">
+        <OwnerInterface />
       </div>
       <div className="mt-20">
         <Results />
