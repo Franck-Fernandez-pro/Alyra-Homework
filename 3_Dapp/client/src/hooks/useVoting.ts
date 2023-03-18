94fbb786bc5b180641290c70f173e8257dce5755
Baptiste <avocalam@gmail.com>
2023-03-18 15:33:52 +0100

    event on add voter and old voter now works

--- 3_Dapp/client/src/hooks/useVoting.ts
+++ 3_Dapp/client/src/hooks/useVoting.ts
@@ -1,11 +1,17 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useContractEvent } from 'wagmi';
 import { useContract, useSigner } from 'wagmi'
 import artifact from '../contracts/Voting.json';
 
 export function useVoting() {
   const [currentWorkflow, setCurrentWorkflow] = useState<number>(0);
+  const [votersAddress, setVotersAddress] = useState<string[]>([]);
+  const [lastAddedVoter, setLastAddedVoter] = useState<string>("");
+  const [userStatus, setUserStatus] = useState<string>("");
   const { data: signerData } = useSigner();
+
+  // @ts-ignore
+  const userAddress = signerData?._address;
   
   useContractEvent({
     address: import.meta.env.VITE_VOTING_ADDR,
@@ -17,11 +23,61 @@ export function useVoting() {
     },
   });
 
+  useContractEvent({
+    address: import.meta.env.VITE_VOTING_ADDR,
+    abi: artifact.abi,
+    eventName: 'VoterRegistered',
+    listener(_, label, __) {
+      //@ts-ignore
+      const newVoter = label?.args?.voterAddress
+      if (!votersAddress.find((elem) => elem == newVoter)) {
+        setLastAddedVoter(newVoter);
+      }
+    },
+    once: true,
+  });
+
   const voting = useContract({
     address: import.meta.env.VITE_VOTING_ADDR,
     abi: artifact.abi,
     signerOrProvider: signerData,
   })
 
-  return { currentWorkflow, voting };
+  useEffect(() => {
+    (async function () {
+        let voterRegisteredFilter = voting?.filters.VoterRegistered();
+        //@ts-ignore
+        let voterRegisteredEvents = await voting?.queryFilter(voterRegisteredFilter);
+        const votersAddressMap = voterRegisteredEvents?.map((elem) => elem?.args?.voterAddress)
+        //@ts-ignore
+        setVotersAddress(votersAddressMap);
+    })();
+  }, [signerData])
+
+  const getUserStatus = async () => {
+    const ownerAddr = await voting?.owner.call();
+
+    if (ownerAddr === userAddress) {
+      setUserStatus("owner");
+      return;
+    }
+
+    if (lastAddedVoter === userAddress) {
+      setUserStatus("voter");
+      return;
+    }
+    if (votersAddress.find((elem) => elem == userAddress)) {
+      setUserStatus("voter");
+      return;
+    }
+
+    setUserStatus("guest");
+  };
+
+  useEffect(() => {
+    getUserStatus();
+  }, [votersAddress, lastAddedVoter]);
+
+
+  return { currentWorkflow, voting, votersAddress, userStatus };
 }
