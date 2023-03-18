742e0750f9228cbeac5a3bb6d3148387b19351b5
Baptiste <avocalam@gmail.com>
2023-03-18 16:27:03 +0100

    change name

--- 3_Dapp/client/src/App.tsx
+++ 3_Dapp/client/src/App.tsx
@@ -2,7 +2,7 @@ import Workflow from './components/Workflow';
 import NavBar from './components/NavBar';
 import ActionsContainer from './components/ActionsContainer';
 import Results from './components/Results';
-import OwnerInterface from './components/OwnerInterface';
+import UtilsInterface from './components/UtilsInterface';
 import { ToastContainer } from 'react-toastify';
 
 function App() {
@@ -18,7 +18,7 @@ function App() {
         <ActionsContainer />
       </div>
       <div className="mt-20">
-        <OwnerInterface />
+        <UtilsInterface />
       </div>
       <div className="mt-20">
         <Results />
