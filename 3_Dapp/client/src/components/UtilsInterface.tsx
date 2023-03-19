cbff77f24b80853793ede2cf2f2529f90df3fa19
Franck Fernandez <franck.fernandez@gmail.com>
2023-03-19 16:06:25 +0100

    Prettier

--- 3_Dapp/client/src/components/UtilsInterface.tsx
+++ 3_Dapp/client/src/components/UtilsInterface.tsx
@@ -2,44 +2,51 @@ import { useState } from 'react';
 import { useVoting } from '../hooks';
 import VotingList from './VotingList';
 
-function UtilsInterface () {
+function UtilsInterface() {
   const { userStatus, nextStep } = useVoting();
-  const [display, setDisplay] = useState<string>("");
+  const [display, setDisplay] = useState<'voters' | 'votes'>('votes');
 
   const handleSeeVoters = () => {
-    setDisplay("voters");
+    setDisplay('voters');
   };
 
   const handleSeeVotes = () => {
-    setDisplay("votes");
+    setDisplay('votes');
   };
 
   const handleNextStep = () => {
-    console.log("next step");
     nextStep();
   };
 
   return (
     <div className="flex justify-center">
-      { (userStatus == "voter" || userStatus == "owner") && (
-        <div className="alert shadow-lg w-1/2 flex justify-around">
-            <label htmlFor="my-modal" onClick={handleSeeVoters} className="btn">Voir les élécteurs</label>
-            <label htmlFor="my-modal" onClick={handleSeeVotes} className="btn">Voir les votes</label>
-            
-            { userStatus === "owner" && 
-              <button onClick={handleNextStep} className="btn btn-secondary">Étape suivante</button>
-            }
+      {(userStatus == 'voter' || userStatus == 'owner') && (
+        <div className="alert flex w-1/2 justify-around shadow-lg">
+          <label htmlFor="my-modal" onClick={handleSeeVoters} className="btn">
+            Voir les élécteurs
+          </label>
+          <label htmlFor="my-modal" onClick={handleSeeVotes} className="btn">
+            Voir les votes
+          </label>
 
-            <input type="checkbox" id="my-modal" className="modal-toggle" />
+          {userStatus === 'owner' && (
+            <button onClick={handleNextStep} className="btn btn-secondary">
+              Étape suivante
+            </button>
+          )}
 
-            <div className="modal">
-              <div className="modal-box">
-                <VotingList dataToDisplay={display}/>
-                <div className="modal-action">
-                  <label htmlFor="my-modal" className="btn">fermer</label>
-                </div>
+          <input type="checkbox" id="my-modal" className="modal-toggle" />
+
+          <div className="modal">
+            <div className="modal-box">
+              <VotingList dataToDisplay={display} />
+              <div className="modal-action">
+                <label htmlFor="my-modal" className="btn">
+                  fermer
+                </label>
               </div>
             </div>
+          </div>
         </div>
       )}
     </div>
--- 3_Dapp/client/src/components/VotingList.tsx
+++ 3_Dapp/client/src/components/VotingList.tsx
@@ -1,48 +1,46 @@
 import { useVoting } from '../hooks';
 
 interface Props {
-  dataToDisplay: string;
+  dataToDisplay: 'voters' | 'votes';
 }
 
 function VotingList({ dataToDisplay }: Props) {
   const { voters } = useVoting();
   const VOTES_TEMPO = [
-    "azerty",
-    "azerty",
-    "azerty",
-    "azerty",
-    "azerty",
-    "azerty"
-  ]
+    'azerty',
+    'azerty',
+    'azerty',
+    'azerty',
+    'azerty',
+    'azerty',
+  ];
 
   return (
-    <div className="flex flex-col justify-center gap-5 w-full">
-      { dataToDisplay === "voters" && (
+    <div className="flex w-full flex-col justify-center gap-5">
+      {dataToDisplay === 'voters' && (
         <>
-          {voters.map((voter) => {
-            return(
-              <div
-                className="badge badge-tertiary cursor-pointer w-full"
-                onClick={() => navigator.clipboard.writeText(voter)}
-              >
-                {voter}
-              </div>
-            )
-          })}
+          {voters.map((voter, idx) => (
+            <div
+              className="badge badge-tertiary w-full cursor-pointer"
+              onClick={() => navigator.clipboard.writeText(voter)}
+              key={idx}
+            >
+              {voter}
+            </div>
+          ))}
         </>
       )}
-      { dataToDisplay === "votes" && (
+      {dataToDisplay === 'votes' && (
         <>
-          {VOTES_TEMPO.map((elem) => {
-            return(
-              <div
-                className="badge badge-tertiary cursor-pointer w-full"
-                onClick={() => navigator.clipboard.writeText(elem)}
-              >
-                {elem}
-              </div>
-            )
-          })}
+          {VOTES_TEMPO.map((vote, idx) => (
+            <div
+              className="badge badge-tertiary w-full cursor-pointer"
+              onClick={() => navigator.clipboard.writeText(vote)}
+              key={idx}
+            >
+              {vote}
+            </div>
+          ))}
         </>
       )}
     </div>
